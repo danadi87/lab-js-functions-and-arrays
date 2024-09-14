7b1b8d544a94cdd799d6ab203c4efeec089f092a
Mirela Dragulescu <dragulescumirela@gmail.com>
2024-09-14 09:53:36 +0200

    Mirela's solution

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,88 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  }
+  {
+    if (num1 < num2) {
+      return num2;
+    }
+  }
+  {
+    return num1 || num2;
+  }
+}
+console.log(maxOfTwoNumbers(3, 3));
+console.log(maxOfTwoNumbers(7, 1));
+console.log(maxOfTwoNumbers(2, 8));
 
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  if (words.length === 0) {
+    return null;
+  }
+  let longestWord = "";
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
 
+  return longestWord;
+}
 
-
+console.log(findLongestWord(words));
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+}
 
-
-
+console.log(sumNumbers(numbers));
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function sumNumbers(numbers2) {
+  let sum = 0;
+  for (let i = 0; i < numbers2.length; i++) {
+    sum += numbers2[i];
+  }
+  return sum;
+}
 
+console.log(sumNumbers(numbers2));
 
-
-
+function averageNumbers(sumNumbers) {
+  return sumNumbers/sumNumbers.length;
+}
+console.log(averageNumbers(numbers2));
 // Iteration 5 | Find Elements
-const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+const words2 = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
 
 function doesWordExist() {}
